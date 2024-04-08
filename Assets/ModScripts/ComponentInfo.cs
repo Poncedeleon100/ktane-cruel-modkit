b83207d579bf51ba4ecdc8d6d3529e88d0d73f36
Poncedeleon100 <ianm0513@comcast.net>
2024-04-07 21:29:54 -0400

    Added randomization to 5 components
    Added randomization to Wires, Button, LEDs, Symbols, and Alphabet.
    Also made some adjustments to the prefab

--- Assets/ModScripts/ComponentInfo.cs
+++ Assets/ModScripts/ComponentInfo.cs
@@ -1,9 +1,102 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using UnityEngine;
+using KModkit;
+using rnd = UnityEngine.Random;
 
-public class ComponentInfo
-{
+public class ComponentInfo {
 
+    //Strings
+    public static readonly string[] DIRNAMES = {"Center", "Up", "Right", "Down", "Left", "Up-Right", "Down-Right", "Down-Left", "Up-Left"};
+    public static readonly string[] SYMBOLCHARS = {"©", "★", "☆", "ټ", "Җ", "Ω", "Ѭ", "Ѽ", "ϗ", "ϫ", "Ϭ", "Ϟ", "Ѧ", "æ", "Ԇ", "Ӭ", "҈", "Ҋ", "Ѯ", "¿", "¶", "Ͼ", "Ͽ", "ψ", "Ѫ", "Ҩ", "҂", "Ϙ", "ζ", "ƛ", "Ѣ", "ע", "⦖", "ኒ", "エ", "π", "Э", "⁋", "ᛤ", "Ƿ", "Щ", "ξ", "Ᵹ", "Ю", "௵", "ϑ", "Triquetra", "ꎵ", "よ"};
+    public static readonly Color[] LIGHTCOLORS = {new Color(1, 0, 0), new Color(0, 0.737f, 0), new Color(0.388f, 0.27f, 1), new Color(1, 1, 0)};
+    public static readonly string[] WireColors = {"Black", "Blue", "Cyan", "Green", "Grey", "Lime", "Orange", "Pink", "Purple", "Red", "White", "Yellow"};
+    public static readonly string[] ButtonList = {"Press", "Hold", "Detonate", "Mash", "Tap", "Push", "Abort", "Button", "Click", "_", "Nothing", "No", "I Don't Know", "Yes"};
+
+    //Colors
+    public Color ButtonTextWhite = new Color(1,1,1);
+
+    //Variables to be passed to cruelModkitScript
+    public int[][] Wires = new int[][] {
+        new int[] {0, 0, 0, 0, 0, 0, 0},
+        new int[] {0, 0, 0, 0, 0, 0, 0},
+    };
+    public int[] WireLED;
+    public string ButtonText;
+    public int Button;
+    public int[] LED;
+    public int[] Symbols;
+    public string[] Alphabet = new string[6];
+    public int[] Arrows;
+
+    public ComponentInfo() {
+        List<int> Temp = new List<int>();
+        //Generate colors for Wires
+        for(int i = 0; i < 7; i++) {
+            int TempColor1 = rnd.Range(0,12);
+            int TempColor2 = rnd.Range(0,12);
+            if(TempColor1 > TempColor2) {
+                int x = TempColor2;
+                TempColor2 = TempColor1;
+                TempColor1 = x;
+            }
+                Wires[0][i] = TempColor1;
+                Wires[1][i] = TempColor2;
+        }
+        //Generate LEDs/Stars for Wire LEDs
+        while(Temp.Count < 7) {
+            int Star = rnd.Range(0,3);
+            int Color = rnd.Range(0,11);
+            int Coefficient = (Star * 11);
+            Color += Coefficient;
+            Temp.Add(Color);
+        }
+        WireLED = Temp.ToArray();
+        //Generate color and text for Button
+        ButtonText = ButtonList[rnd.Range(0,14)];
+        Button = rnd.Range(0,11);
+        //Generate colors for LEDs
+        Temp.Clear();
+        while(Temp.Count < 8) {
+            int Color = rnd.Range(0,11);
+            Temp.Add(Color);
+        }
+        LED = Temp.ToArray();
+        //Generate symbols
+        Temp.Clear();
+        while(Temp.Count < 6) {
+            int Symbol = rnd.Range(0,49);
+            if(!Temp.Contains(Symbol))
+                Temp.Add(Symbol);
+        }
+        Symbols = Temp.ToArray();
+        //Generate Alphabet text
+        for(int i = 0; i < 6; i++) {
+            string AlphabetKey = System.String.Empty;
+            string[] Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray().Select(x => x.ToString()).OrderBy(x => rnd.Range(0, 1000)).ToArray();
+            string[] Numbers = "1234567890".ToCharArray().Select(x => x.ToString()).OrderBy(x => rnd.Range(0, 1000)).ToArray();
+            int LetterAmount = rnd.Range(0,2);
+            int NumberAmount = rnd.Range(0,2);
+            for(int x = 0; x <= LetterAmount; x++) {
+                AlphabetKey += Letters[x];
+            }
+            if(LetterAmount == 1 && NumberAmount == 1) {
+                AlphabetKey += Environment.NewLine;
+            }
+            for(int x = 0; x <= NumberAmount; x++) {
+                AlphabetKey += Numbers[x];
+            }
+            Alphabet[i] = AlphabetKey;
+        }
+        //Generate arrow colors
+        //Generate Identity information
+        //Generate Bulb colors and button labels
+        //Generate text and colors for Resistor
+        //Generate timer text
+        //Generate word display text
+        //Generate number display text
+        //Generate morse code display
+    }
 }
--- Assets/The Cruel Modkit/cruelModkitScript.cs
+++ Assets/The Cruel Modkit/cruelModkitScript.cs
@@ -9,28 +9,76 @@ public class cruelModkitScript : MonoBehaviour {
 
 	public KMAudio Audio;
 	public KMBombInfo Bomb;
+	public KMBombModule Module;
 
+	//Component Selector
 	public GameObject[] Doors;
 	public GameObject[] Components;
 	public KMSelectable[] SelectorButtons;
 	public KMSelectable[] UtilityButton;
 	public TextMesh DisplayText;
 
+	//Materials
 	public Material[] WireMats;
-	// public Material[] WireLEDMats;
+	public Material[] WireLEDMats;
+	public Material[] ButtonMats;
+	public Material[] LEDMats;
+	public Material[] KeyLightMats;
+	public Material[] SymbolMats;
+	public Material[] ArrowMats;
+	public Material[] IdentityMats;
+	//public TextMesh[] IdentityText;
+	//public Light[] BulbLights;
+	//public MeshRenderer[] BulbGlass;
+	//public GameObject[] BulbFilaments;
+	//public Transform[] BulbFaces;
+	public Material[] ResistorMats;
+	//public TextMesh[] ResistorText;
+	//public TextMesh[] WidgetText;
+	public Material[] MorseMats;
+	public Material[] MeterMats;
 
+	public GameObject[] Wires;
+	public GameObject[] WireLED;
+	public TextMesh ButtonText;
+	public Renderer Button;
+	public GameObject[] Adventure;
+	public GameObject[] LED;
+	public GameObject[] Symbols;
+	public GameObject[] Alphabet;
+	public GameObject[] Piano;
+	public GameObject[] Arrows;
+	public Transform ArrowsBase;
+	public GameObject[] Identity;
+	public GameObject[] Bulbs;
+	public GameObject[] Resistor;
+
+	public Mesh[] WireMesh;
+	List<int> WiresCut = new List<int>();
+
+	//Component Selector Info
 	readonly string[] ComponentNames = new string[] {"WIRES", "BUTTON", "ADVENTURE", "LED", "SYMBOLS", "ALPHABET", "PIANO", "ARROWS", "IDENTITY", "BULBS", "RESISTOR"};
-	bool[] OnComponents = new bool[] {false, false, false, false, false, false, false, false, false, false, false};
+	bool[] OnComponents = new bool[11];
+	bool[] TargetComponents = new bool[11];
 	int CurrentComponent = 0;
+
+	ComponentInfo Info;
+	Puzzle Puzzle;
 
 	// Logging
 	static int ModuleIdCounter = 1;
 	int ModuleId;
 	private bool ModuleSolved;
-    private bool Animating = false;
-    private bool Solving = false;
+    private bool Animating;
+    private bool Solving;
 
-	private bool ForceComponents, ForceByModuleID = false;
+	// private bool HasStruck = false; // TP Handling, send a strike handling if the module struck. To prevent excessive inputs.
+
+	// Use these for debugging individual puzzles.
+	private bool ForceComponents, ForceByModuleID;
+	/*/private bool[] componentsForced;
+	public bool enableBruteTest = false;
+	ModkitSettings modConfig = new ModkitSettings();/*/
 
 	void Awake () {
 		ModuleId = ModuleIdCounter++;
@@ -46,6 +94,23 @@ public class cruelModkitScript : MonoBehaviour {
 			ChangeDisplayComponent(SelectorButtons[2], 1);
 			return false;
 		};
+	}
+
+	void Start () {
+		SetUpComponents();
+		if (ForceComponents) { // Check if the components need to be forced on.
+			// ForceComponents();
+			// DisplayText.text = "DISABLED";
+		}
+		else {
+			// CalcComponents();
+			DisplayText.text = ComponentNames[CurrentComponent];
+		}
+	}
+
+	// Triggered once per frame
+	void Update () {
+		// Ex: How many modules have been solved?
 	}
 
 	void ChangeDisplayComponent(KMSelectable Button, int i) {
@@ -86,34 +151,53 @@ public class cruelModkitScript : MonoBehaviour {
 		}
 	}
 
-	public IEnumerator AnimateButtonPress(Transform Object, Vector3 Offset)
-    {
-        for (int x = 0; x < 5; x++)
-        {
+	public IEnumerator AnimateButtonPress(Transform Object, Vector3 Offset) {
+        for (int x = 0; x < 5; x++) {
 			Object.localPosition += Offset / 5;
 			yield return new WaitForSeconds(0.01f);
         }
-        for (int x = 0; x < 5; x++)
-        {
+        for (int x = 0; x < 5; x++) {
 			Object.localPosition -= Offset / 5;
 			yield return new WaitForSeconds(0.01f);
         }
     }
 
-	void Start () {
-		if (ForceComponents) { // Check if the components need to be forced on.
-			// ForceComponents();
-			// displayText.text = "DISABLED";
+	void SetUpComponents() {
+		Info = new ComponentInfo();
+		//Set materials for Wires
+		for(int i = 0; i < 7; i++) {
+			int Color1 = Info.Wires[0][i];
+			int Color2 = Info.Wires[1][i];
+			if(Color1 != Color2) {
+				Wires[i].transform.GetComponentInChildren<Renderer>().material = WireMats.Where(x => x.name == ComponentInfo.WireColors[Color1] + "_" + ComponentInfo.WireColors[Color2]).ToArray()[0];
+			}
+			else {
+				Wires[i].transform.GetComponentInChildren<Renderer>().material = WireMats.Where(x => x.name == ComponentInfo.WireColors[Color1]).ToArray()[0];
+			}
 		}
-		else
+		//Set materials for Wire LEDs
+		for(int i = 0; i < WireLED.Length; i++)
 		{
-			// CalcComponents();
-			DisplayText.text = ComponentNames[CurrentComponent];
+			WireLED[i].transform.Find("WireLEDL").GetComponentInChildren<Renderer>().material = WireLEDMats[Info.WireLED[i]];
 		}
-	}
-	
-	// Triggered once per frame
-	void Update () {
-		// Ex: How many modules have been solved?
+		//Set text and material for Button
+		ButtonText.text = Info.ButtonText;
+		Button.material = ButtonMats[Info.Button];
+		if(Info.Button == 0 || Info.Button == 1) {
+			ButtonText.color = Info.ButtonTextWhite;
+		}
+		//Set materials for LEDs
+		for(int i = 0; i < LED.Length; i++)
+		{
+			LED[i].transform.Find("LEDL").GetComponentInChildren<Renderer>().material = LEDMats[Info.LED[i]];
+		}
+		//Set materials for Symbols
+		for(int i = 0; i < Symbols.Length; i++) {
+			Symbols[i].transform.Find("Symbol").GetComponentInChildren<Renderer>().material = SymbolMats[Info.Symbols[i]];
+		}
+		//Set Alphabet text
+		for(int i = 0; i < Alphabet.Length; i++) {
+			Alphabet[i].transform.Find("AlphabetText").GetComponentInChildren<TextMesh>().text = Info.Alphabet[i];
+		}
 	}
 }
