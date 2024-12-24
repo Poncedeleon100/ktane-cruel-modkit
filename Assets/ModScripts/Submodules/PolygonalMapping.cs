d71a975b2c06e90dd18017cc87cb5b8ebdacca37
PossessedHood416 <165974543+PossessedHood416@users.noreply.github.com>
2024-12-24 11:36:30 -0500

    v1.0.4
    Tweaked polygon generation
    Minor optimizations
    Loosely explained the ReturnPolygonVertexes function (ping me if it's still confusing)
    Tweaked manual so no conflicts
    Typos in logging
    Comply with Gameplushy's format
    And a couple more minor things

--- Assets/ModScripts/Submodules/PolygonalMapping.cs
+++ Assets/ModScripts/Submodules/PolygonalMapping.cs
@@ -6,14 +6,13 @@ using Random = UnityEngine.Random;
 
 public class PolygonalMapping : Puzzle
 {
-
     readonly string Base36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string IgnoreString;
-    List<string> FinalOrder = new List<string>();
-    Vector2[] Coordinates = new Vector2[6];
-    bool RanOutOfAttempts = false;
+    readonly List<string> FinalOrder = new List<string>();
+    readonly Vector2[] Coordinates = new Vector2[6];
+    readonly bool RanOutOfAttempts = false;
     int RooAMashCount = 0;
-    bool[] IsDepressed = new bool[12];
+    readonly bool[] IsDepressed = new bool[12];
 
     readonly int[,] BigTable = new int[26, 10] {
         {38, 18, 19, 17, 30, 8,  17, 26, 32, 26},
@@ -44,28 +43,27 @@ public class PolygonalMapping : Puzzle
         {28, 4,  27, 6,  4,  44, 11, 8,  16, 44}
     };
 
-    public PolygonalMapping(CruelModkitScript Module, int ModuleID, ComponentInfo Info, byte Components) : base(Module, ModuleID, Info, Components)
-    {
+    public PolygonalMapping(CruelModkitScript Module, int ModuleID, ComponentInfo Info, byte Components) : base(Module, ModuleID, Info, Components){
         Debug.LogFormat("[The Cruel Modkit #{0}] Solving Polygonal Mapping. Submodule is preparing itself.", ModuleID);
 
-        //Setup polygon
-        for(int i = 0; i < 6; i++) RegenAlphabetLabel(i); //also puts them in Coordinates array
+        //Setup polygon; put inital alphabet buttons into coordinates array
+        for(int i = 0; i < 6; i++){
+            string AlphabetKey = Info.Alphabet[i];
+            Coordinates[i] = new Vector2(Base36.IndexOf(AlphabetKey[0]) - 10, Base36.IndexOf(AlphabetKey[AlphabetKey.Length-1]));
+        }
+
         int Attempts = 1;
         int AttemptCap = 300;
+        bool IsDupeReal = CoordinatesContainDupe();
 
-        while( ! (ReturnPolygonVertexes(Coordinates).Length == 6 && CoordinatesFindDupe() == -1) && Attempts < AttemptCap){
-            if(CoordinatesFindDupe() != -1){
-                RegenAlphabetLabel(CoordinatesFindDupe());
-            } else {
-                if(Attempts %20 == 0){ //regens first three incase they dont work well
-                    RegenAlphabetLabel(0);
-                    RegenAlphabetLabel(1);
-                    RegenAlphabetLabel(2);
-                }
-                RegenAlphabetLabel(3);
-                RegenAlphabetLabel(4);
-                RegenAlphabetLabel(5);
-            }
+        //generate polygons until a good one exists or ~300 attempts
+        while( (IsDupeReal || ReturnPolygonVertexes(Coordinates).Length != 6) && Attempts < AttemptCap){
+            if(IsDupeReal)
+                CoordinatesReplaceDupe();
+            else
+                for(int i = 0; i < 6; i++) RegenAlphabetLabel(i);
+
+            IsDupeReal = CoordinatesContainDupe();
             Attempts++;
         }
 
@@ -79,18 +77,11 @@ public class PolygonalMapping : Puzzle
         }
 
         //polygon generated well
-        Debug.LogFormat("[The Cruel Modkit #{0}] Success on attempt {1}.", ModuleID, Attempts);
         Debug.LogFormat("[The Cruel Modkit #{0}] Alphabet buttons are: [{1}]", ModuleID, Info.GetAlphabetInfo());
         Debug.LogFormat("[The Cruel Modkit #{0}] Symbols are: [{1}]", ModuleID, Info.GetSymbolInfo());
 
-        string WordDisplayFixed = "";
-        for(int i = 0; i < Info.WordDisplay.Length; i++){
-            if(Base36.IndexOf(Info.WordDisplay[i]) != -1){
-                WordDisplayFixed += Info.WordDisplay[i];
-            }
-        }
-        IgnoreString = Info.Morse + Info.TimerDisplay + Info.ResistorText[0] + Info.ResistorText[1] +
-                            Info.ResistorText[2] + Info.ResistorText[3] + Info.NumberDisplay + WordDisplayFixed + "543210";
+        string WordDisplayFixed = Info.WordDisplay.Where(n => char.IsLetterOrDigit(n)).Join(string.Empty);
+        IgnoreString = Info.Morse + Info.TimerDisplay + Info.ResistorText[0] + Info.ResistorText[1] + Info.ResistorText[2] + Info.ResistorText[3] + Info.NumberDisplay + WordDisplayFixed + "543210";
         Debug.LogFormat("[The Cruel Modkit #{0}] The string obtained from the widgets is \"{1}\".", ModuleID, IgnoreString);
 
         Debug.LogFormat("[The Cruel Modkit #{0}] Calculating which buttons should be pressed, 0-indexed.", ModuleID);
@@ -113,7 +104,8 @@ public class PolygonalMapping : Puzzle
                     TestCoordinate = new Vector2(x,y);
                     if( ! Info.Symbols.Contains( BigTable[(int)TestCoordinate.x, (int)TestCoordinate.y])) continue;
                     if( ! IsInPolygon(TestCoordinate)) continue;
-                    int i = Array.IndexOf(Info.Symbols, BigTable[(int)TestCoordinate.x, (int)TestCoordinate.y] ); 
+                    int i = Array.IndexOf(Info.Symbols, BigTable[(int)TestCoordinate.x, (int)TestCoordinate.y] );
+                    if(FinalOrder.Contains("S"+i)) continue;
                     SymbolCounter[i]++;
                     Debug.LogFormat("[The Cruel Modkit #{0}] Found symbol {1} ({4}) at {2}{3}).", ModuleID, i, Base36[(int)TestCoordinate.x+10], (int)TestCoordinate.y, Info.Symbols[i]);                    
                 }
@@ -139,12 +131,12 @@ public class PolygonalMapping : Puzzle
                     break;
                 }
 
-                Debug.LogFormat("[The Cruel Modkit #{0}] Current highest is index {1} at {2} entries", ModuleID, HighestIndex, HighestCount);
+                Debug.LogFormat("[The Cruel Modkit #{0}] Current highest is index {1} at {2} entries.", ModuleID, HighestIndex, HighestCount);
 
                 //symbol press
                 if(!FinalOrder.Contains("S" + HighestIndex.ToString())){
                     FinalOrder.Add("S" + HighestIndex.ToString());
-                    Debug.LogFormat("[The Cruel Modkit #{0}] You should press Symbol {1}", ModuleID, HighestIndex);
+                    Debug.LogFormat("[The Cruel Modkit #{0}] You should press Symbol {1}.", ModuleID, HighestIndex);
                 }
 
                 SymbolCounter[HighestIndex] = -1;
@@ -160,8 +152,8 @@ public class PolygonalMapping : Puzzle
             }
 
             FinalOrder.Add("A" + j.ToString());
-            Debug.LogFormat("[The Cruel Modkit #{0}] You should press Alphabet {1}", ModuleID, j);
-            Debug.LogFormat("[The Cruel Modkit #{0}] Ignore string is currently \"{1}\"", ModuleID, IgnoreString, j.ToString());
+            Debug.LogFormat("[The Cruel Modkit #{0}] You should press Alphabet {1}.", ModuleID, j);
+            Debug.LogFormat("[The Cruel Modkit #{0}] Ignore string is currently \"{1}\".", ModuleID, IgnoreString, j.ToString());
 
             //alter coords array to ignore (another) alphabet button
             Coordinates[j] = new Vector2(777,777);
@@ -363,61 +355,84 @@ public class PolygonalMapping : Puzzle
 
     }
 
-    string ReturnPolygonVertexes(Vector2[] Vertexes){
-        string VertexList = "";
-        //find first label in reverse chinese order bc of how atan2 works
-        int FirstIndex = 0;
-        int FirstPosition = 261;
-        for(int i = 0; i < Vertexes.Length; i++){
-            if(Vertexes[i].x*10 + (10- Vertexes[i].y) < FirstPosition){
-                FirstPosition = (int)(Vertexes[i].x*10 + (10- Vertexes[i].y));
-                FirstIndex = i;
+    //this function makes the largest strictly convex polygon with the given vertex coords, then returns the vertexes used to make that polygon
+    //strictly convex means that the polygon is easier for the expert to construct and doesnt cause weird jank when vertexes are removed
+    string ReturnPolygonVertexes(Vector2[] vertexes){
+        string vertexList = "";
+
+        //find first vertex in reverse chinese order bc of how atan2 works
+        int firstIndex = 0;
+        int firstPosition = 261;
+        for(int i = 0; i < vertexes.Length; i++){
+            if(vertexes[i].x*10 + (10- vertexes[i].y) < firstPosition){
+                firstPosition = (int)(vertexes[i].x*10 + (10- vertexes[i].y));
+                firstIndex = i;
             }
         }
 
-        int NextIndex = 999;
-        int CurrentIndex = FirstIndex;
-        double MaxAtan2 = -4;
-        double Atan2StepCap = 4;
+        int currentIndex = firstIndex;
+        int nextIndex = 999;
+        double largestAtan2 = -4;
+        double atan2StepCap = 4;
 
-        while(NextIndex != FirstIndex && VertexList.Length != 8){
-            for(int i = 0; i < Vertexes.Length; i++){
-                if(i == CurrentIndex) continue;
-                if((int)Vertexes[i].x == 777) continue;
-                double CurrentAtan2 = ReturnAtan2(Vertexes[CurrentIndex], Vertexes[i]);
-                if(CurrentAtan2 == MaxAtan2) return "6";
+        //itteratively goes around the vertexes to form polygon, starting at firstIndex vertex going south,counter
+        while(nextIndex != firstIndex && vertexList.Length != 8){
+            for(int i = 0; i < vertexes.Length; i++){
+                
+                //dont compare a coord to itself; ignore coords with a dummy value
+                if(i == currentIndex) continue; 
+                if((int)vertexes[i].x == 777) continue;
+                
+                double currentAtan2 = ReturnAtan2(vertexes[currentIndex], vertexes[i]);
+                
+                if(currentAtan2 == largestAtan2) return "6";
+                //this only occurs when there is a straight line of 3 or more symbols
+                //returning "6" is just the easiest way to do what i want whenever this function is called
 
-                if(CurrentAtan2 > MaxAtan2 && CurrentAtan2 < Atan2StepCap){
-                    NextIndex = i;
-                    MaxAtan2 = CurrentAtan2;
+                //currentAtan2 < atan2StepCap ensures that the method goes counter only
+                if(currentAtan2 > largestAtan2 && currentAtan2 < atan2StepCap){
+                    nextIndex = i;
+                    largestAtan2 = currentAtan2;
                 }
             }
 
-            VertexList+= NextIndex;
-            Atan2StepCap = MaxAtan2;
-            CurrentIndex = NextIndex;
-            MaxAtan2 = -4;
+            //setup for next itteration
+            vertexList+= nextIndex;
+            atan2StepCap = largestAtan2;
+            currentIndex = nextIndex;
+            largestAtan2 = -4;
         }
-        return VertexList;
+        //feel free to ping Possessed on stuff that still doesn't make sense
+        return vertexList;
     }
 
     bool IsInPolygon(Vector2 SymbCoord){
-        Vector2[] CoordinatesAndTest = new Vector2[7];
-        Array.Copy(Coordinates, CoordinatesAndTest, 6);
-        CoordinatesAndTest[6] = new Vector2(SymbCoord.x, SymbCoord.y);
-        if(ReturnPolygonVertexes(CoordinatesAndTest).IndexOf("6") == -1)
+        Vector2[] coordinatesAndTest = new Vector2[7];
+        Array.Copy(Coordinates, coordinatesAndTest, 6);
+        coordinatesAndTest[6] = new Vector2(SymbCoord.x, SymbCoord.y);
+        if(ReturnPolygonVertexes(coordinatesAndTest).IndexOf("6") == -1)
             return true;
         else
             return false;
 
     }
 
-    int CoordinatesFindDupe(){
-        HashSet<Vector2> DistinctCoordinates = new HashSet<Vector2>();
-        for(int i = 0; i < 6; i++)
-            if(!DistinctCoordinates.Add(Coordinates[i]))
-                return i;
-        return -1;
+    bool CoordinatesContainDupe(){
+        if(Coordinates.Distinct().ToArray().Length == Coordinates.Length) return false;
+        return true;
+    }
+
+    void CoordinatesReplaceDupe(){
+        if(!CoordinatesContainDupe()) return;
+
+        HashSet<Vector2> distinctCoordinates = new HashSet<Vector2>();
+        for(int i = 0; i < 6; i++){
+            if(!distinctCoordinates.Add(Coordinates[i])){
+                RegenAlphabetLabel(i);
+                i--;
+            }
+        }
+        return;
     }
 
     double ReturnAtan2(Vector2 From, Vector2 To){
@@ -425,22 +440,22 @@ public class PolygonalMapping : Puzzle
     }
 
     void RegenAlphabetLabel(int i){
-        string AlphabetKey = String.Empty;
-        string[] Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray().Select(x => x.ToString()).OrderBy(x => Random.Range(0, 1000)).ToArray();
-        string[] Numbers = "1234567890".ToCharArray().Select(x => x.ToString()).OrderBy(x => Random.Range(0, 1000)).ToArray();
-        int LetterAmount = Random.Range(0, 2);
-        int NumberAmount = Random.Range(0, 2);
-        for (int x = 0; x <= LetterAmount; x++) AlphabetKey += Letters[x];
-        if (LetterAmount == 1 && NumberAmount == 1) AlphabetKey += Environment.NewLine;
-        for (int x = 0; x <= NumberAmount; x++) AlphabetKey += Numbers[x];
-        Info.Alphabet[i] = AlphabetKey;
-        Module.Alphabet[i].transform.Find("AlphabetText").GetComponentInChildren<TextMesh>().text = AlphabetKey;
-        Coordinates[i] = new Vector2(Base36.IndexOf(AlphabetKey[0]) - 10, Base36.IndexOf(AlphabetKey[AlphabetKey.Length-1]));
+        string alphabetKey = String.Empty;
+        string[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray().Select(x => x.ToString()).OrderBy(x => Random.Range(0, 1000)).ToArray();
+        string[] numbers = "1234567890".ToCharArray().Select(x => x.ToString()).OrderBy(x => Random.Range(0, 1000)).ToArray();
+        int letterAmount = Random.Range(0, 2);
+        int numberAmount = Random.Range(0, 2);
+        for (int x = 0; x <= letterAmount; x++) alphabetKey += letters[x];
+        if (letterAmount == 1 && numberAmount == 1) alphabetKey += Environment.NewLine;
+        for (int x = 0; x <= numberAmount; x++) alphabetKey += numbers[x];
+        Info.Alphabet[i] = alphabetKey;
+        Module.Alphabet[i].transform.Find("AlphabetText").GetComponentInChildren<TextMesh>().text = alphabetKey;
+        Coordinates[i] = new Vector2(Base36.IndexOf(alphabetKey[0]) - 10, Base36.IndexOf(alphabetKey[alphabetKey.Length-1]));
     }
     
-    bool CoordinateLiesOnLine (Vector2 AlphaCoord1, Vector2 AlphaCoord2, Vector2 TestCoord){
-        if(TestCoord == AlphaCoord1 || TestCoord == AlphaCoord2) return false;
-        if(ReturnAtan2(AlphaCoord1, AlphaCoord2) == ReturnAtan2(AlphaCoord1, TestCoord) && ReturnAtan2(AlphaCoord1, AlphaCoord2) == ReturnAtan2(TestCoord, AlphaCoord2))
+    bool CoordinateLiesOnLine (Vector2 alphaCoord1, Vector2 alphaCoord2, Vector2 testCoord){
+        if(testCoord == alphaCoord1 || testCoord == alphaCoord2) return false;
+        if(ReturnAtan2(alphaCoord1, alphaCoord2) == ReturnAtan2(alphaCoord1, testCoord) && ReturnAtan2(alphaCoord1, alphaCoord2) == ReturnAtan2(testCoord, alphaCoord2))
             return true;
 
         return false;
